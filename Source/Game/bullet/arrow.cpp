9a17c6b5aa57555a00dea85c3498efe3f322be66
CalvinWan <t110820058@ntut.org.tw>
2024-03-22 10:11:46 +0800

    [Feature Addition] Added bad bullet resources.

--- Source/Game/bullet/arrow.cpp
+++ Source/Game/bullet/arrow.cpp
@@ -7,7 +7,7 @@ Arrow::Arrow() {
 void Arrow::Start() {
     vector<string> filepaths;
     for (int i = 0; i <= 360; i += 3) {
-        filepaths.push_back("Resources/weapon/bad_pistol/" + to_string(i) + ".bmp");
+        filepaths.push_back("Resources/bullet/bad_bullet/" + to_string(i) + ".bmp");
     }
     SetImages(filepaths, RGB(255, 255, 255));
 }
