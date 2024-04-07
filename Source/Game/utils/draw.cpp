1119f5ff0dc26153fcfb14517da7a538c6bdb707
ba_la_ba_la <evictor92@gmail.com>
2024-04-07 22:50:36 +0800

    [Feature Addition] Added Text method in Draw.

--- Source/Game/utils/draw.cpp
+++ Source/Game/utils/draw.cpp
@@ -8,6 +8,7 @@
 #include "draw_command.h"
 #include "draw_empty_rectangle_command.h"
 #include "draw_rectangle_command.h"
+#include "draw_text_command.h"
 
 
 namespace game_framework {
@@ -28,6 +29,11 @@ namespace game_framework {
     void Draw::EmptyRectangle(Point point1, Point point2, COLORREF color, Point radius, int thickness) {
         this->drawCommands.emplace_back(new DrawEmptyRectangleCommand(point1, point2, color, radius, thickness));
     }
+
+    void Draw::Text(Point point, int size, COLORREF color, std::string str, std::string fontName) {
+        this->drawCommands.emplace_back((new DrawTextCommand(point, size, color, str, fontName)));
+    }
+
 
     void Draw::Show() {
         CDC *cdc = CDDraw::GetBackCDC(); // get CDC
