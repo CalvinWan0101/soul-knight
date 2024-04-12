66ac4b7f8bc8e8737a047b49772c50b198b25965
ba_la_ba_la <evictor92@gmail.com>
2024-04-12 11:37:11 +0800

    [Feature Addition] Created abstract class Stage.

---
+++ Source/Game/stage/stage.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Stage {
+public:
+
+private:
+    int room;
+    void GenerateWall();
+    void SetPlayerPosition();
+    void SetTransferGatePosition();
+    void GenerateObstacle();
+};
