266635dd24613940fb455d5c205a88e4db7dc09a
ba_la_ba_la <evictor92@gmail.com>
2024-04-26 11:37:58 +0800

    [Refactoring] Made RotatableObject to inherit GameObject instead of GeneralObject.

--- Source/Game/utils/rotatable_object.cpp
+++ Source/Game/utils/rotatable_object.cpp
@@ -12,30 +12,41 @@ RotatableObject::RotatableObject() {
 }
 
 void RotatableObject::Start() {
-    GeneralObject::Start();
+    GameObject::Start();
 }
 
 void RotatableObject::Update() {
-    GeneralObject::Update();
+    GameObject::Update();
+}
+
+void RotatableObject::Show(Point screenPositoin) {
+    SetFrameIndexOfBitmap(rotation.GetRadian());
+    double screenX = this->position.GetX() - screenPositoin.GetX();
+    double screenY = this->position.GetY() - screenPositoin.GetY();
+    cMovingBitmap.SetTopLeft(static_cast<int>(screenX - centerOffset.GetX()),
+                                static_cast<int>(screenY - centerOffset.GetY()));
+    cMovingBitmap.ShowBitmap();
 }
 
 void RotatableObject::Rotate(double radian) {
     rotation.Rotate(radian);
-    SetFrameIndexOfBitmap(rotation.GetRadian());
 }
 
 void RotatableObject::SetRotation(Vec* radian) {
     rotation = Vec(radian);
-    SetFrameIndexOfBitmap(rotation.GetRadian());
 }
 
 void RotatableObject::SetRotation(double radian) {
     rotation.Rotate(radian - rotation.GetRadian());
-    SetFrameIndexOfBitmap(rotation.GetRadian());
 }
 
 Vec RotatableObject::GetRotation() const {
     return rotation;
+}
+
+void RotatableObject::SetHitBoxByRatio(double heightRatio, double widthRatio) {
+    hitBox.SetHeight(static_cast<double>(cMovingBitmap.GetHeight()) * heightRatio);
+    hitBox.SetWidth(static_cast<double>(cMovingBitmap.GetWidth()) * widthRatio);
 }
 
 void RotatableObject::SetImages(string filePath, int index, COLORREF color) {
@@ -43,11 +54,15 @@ void RotatableObject::SetImages(string filePath, int index, COLORREF color) {
     for (int i = 0; i <= 360; i += index) {
         filepaths.push_back(filePath + to_string(i) + ".bmp");
     }
-    game_framework::CMovingBitmap cMovingBitmap = game_framework::CMovingBitmap();
+    cMovingBitmap = game_framework::CMovingBitmap();
     cMovingBitmap.LoadBitmapByString(filepaths, color);
-    cMovingBitmaps.push_back(cMovingBitmap);
 }
 
 void RotatableObject::SetFrameIndexOfBitmap(double radian) {
-    cMovingBitmaps[0].SetFrameIndexOfBitmap(static_cast<int>((radian / (2 * PI) ) * static_cast<double>(cMovingBitmaps[0].GetFrameSizeOfBitmap())));
+    cMovingBitmap.SetFrameIndexOfBitmap(static_cast<int>((radian / (2 * PI) ) * static_cast<double>(cMovingBitmap.GetFrameSizeOfBitmap())));
 }
+
+void RotatableObject::SetCenter() {
+    this->centerOffset.SetVec(cMovingBitmap.GetWidth() / 2, cMovingBitmap.GetHeight() / 2);
+}
+
--- Source/Game/utils/rotatable_object.h
+++ Source/Game/utils/rotatable_object.h
@@ -7,18 +7,24 @@
 #include "general_object.h"
 #include "vec.h"
 
-class RotatableObject : public GeneralObject {
+class RotatableObject : public GameObject {
 public:
     RotatableObject();
     void Start() override;
     void Update() override;
+    void Show(Point screenPositoin) override;
     void Rotate(double radian);
     void SetRotation(Vec* radian);
     void SetRotation(double radian);
     Vec GetRotation() const;
+    
+    void SetHitBoxByRatio(double heightRatio, double widthRatio) override;
 
 protected:
     Vec rotation;
     void SetImages(string filePath, int index, COLORREF color);
     void SetFrameIndexOfBitmap(double radian);
+    void SetCenter() override;
+private:
+    game_framework::CMovingBitmap cMovingBitmap;
 };
