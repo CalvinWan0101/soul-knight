29452606f8ebedbb5859805891bfb35f80e643ba
CalvinWan <t110820058@ntut.org.tw>
2024-05-12 03:57:53 +0800

    [Feature Addition] Finished ProjectilePool.

--- Source/Game/pool/ProjectilePool.cpp
+++ Source/Game/pool/ProjectilePool.cpp
@@ -1,7 +1,8 @@
 ﻿#include "stdafx.h"
 #include "ProjectilePool.h"
 
-#define PREALLOCATE_PROJECTILES 50
+#define PREALLOCATE_BULLET 50
+#define PREALLOCATE_SHOCK_WAVE 10
 
 ProjectilePool* ProjectilePool::instance = nullptr;
 
@@ -13,15 +14,56 @@ ProjectilePool* ProjectilePool::Instance() {
 }
 
 ProjectilePool::ProjectilePool() {
-    for (int i = 0; i < PREALLOCATE_PROJECTILES; i++) {
+    // bullet
+    for (int i = 0; i < PREALLOCATE_BULLET; i++) {
+        arrows.push_back(new Arrow());
+    }
+    for (int i = 0; i < PREALLOCATE_BULLET; i++) {
         badPistolBullets.push_back(new BadPistolBullet());
+    }
+    // shock wave
+    for (int i = 0; i < PREALLOCATE_SHOCK_WAVE; i++) {
+        hammerWaves.push_back(new HammerWave());
+    }
+    // other
+    for (int i = 0; i < PREALLOCATE_SHOCK_WAVE; i++) {
+        invisibleShockWaves.push_back(new InvisibleShockWave());
     }
 }
 
 ProjectilePool::~ProjectilePool() {
+    // bullet
+    for (Arrow* arrow : arrows) {
+        delete arrow;
+    }
     for (BadPistolBullet* bullet : badPistolBullets) {
         delete bullet;
     }
+    
+    // shock wave
+    for (HammerWave* hammerWave : hammerWaves) {
+        delete hammerWave;
+    }
+    
+    // other
+    for (InvisibleShockWave* invisibleShockWave : invisibleShockWaves) {
+        delete invisibleShockWave;
+    }
+}
+
+// bullet
+Arrow* ProjectilePool::AcquireArrow() {
+    if (arrows.empty()) {
+        arrows.push_back(new Arrow());
+    }
+    Arrow* arrow = arrows.back();
+    arrows.pop_back();
+    return arrow;
+}
+
+void ProjectilePool::ReleaseArrow(Arrow* arrow) {
+    arrow->RemoveTag(Tag::REMOVE_ON_NEXT_FRAME);
+    arrows.push_back(arrow);
 }
 
 BadPistolBullet* ProjectilePool::AcquireBadPistolBullet() {
@@ -29,11 +71,41 @@ BadPistolBullet* ProjectilePool::AcquireBadPistolBullet() {
         badPistolBullets.push_back(new BadPistolBullet());
     }
     BadPistolBullet* bullet = badPistolBullets.back();
-    bullet->RemoveTag(Tag::REMOVE_ON_NEXT_FRAME);
     badPistolBullets.pop_back();
     return bullet;
 }
 
 void ProjectilePool::ReleaseBadPistolBullet(BadPistolBullet* bullet) {
+    bullet->RemoveTag(Tag::REMOVE_ON_NEXT_FRAME);
     badPistolBullets.push_back(bullet);
+}
+
+// shock wave
+HammerWave* ProjectilePool::AcquireHammerWave() {
+    if (hammerWaves.empty()) {
+        hammerWaves.push_back(new HammerWave());
+    }
+    HammerWave* hammerWave = hammerWaves.back();
+    hammerWaves.pop_back();
+    return hammerWave;
+}
+
+void ProjectilePool::ReleaseHammerWave(HammerWave* hammerWave) {
+    hammerWave->RemoveTag(Tag::REMOVE_ON_NEXT_FRAME);
+    hammerWaves.push_back(hammerWave);
+}
+
+// other
+InvisibleShockWave* ProjectilePool::AcquireInvisibleShockWave() {
+    if (invisibleShockWaves.empty()) {
+        invisibleShockWaves.push_back(new InvisibleShockWave());
+    }
+    InvisibleShockWave* invisibleShockWave = invisibleShockWaves.back();
+    invisibleShockWaves.pop_back();
+    return invisibleShockWave;
+}
+
+void ProjectilePool::ReleaseInvisibleShockWave(InvisibleShockWave* invisibleShockWave) {
+    invisibleShockWave->RemoveTag(Tag::REMOVE_ON_NEXT_FRAME);
+    invisibleShockWaves.push_back(invisibleShockWave);
 }
--- Source/Game/pool/ProjectilePool.h
+++ Source/Game/pool/ProjectilePool.h
@@ -1,15 +1,39 @@
 ﻿#pragma once
+#include "../projectile/InvisibleShockWave.h"
+#include "../projectile/bullet/Arrow.h"
 #include "../projectile/bullet/BadPistolBullet.h"
+#include "../projectile/shock_wave/HammerWave.h"
 
 class ProjectilePool {
 public:
     static ProjectilePool* Instance();
     ProjectilePool();
     ~ProjectilePool();
+
+    // bullet
+    Arrow* AcquireArrow();
+    void ReleaseArrow(Arrow* arrow);
+    
     BadPistolBullet* AcquireBadPistolBullet();
     void ReleaseBadPistolBullet(BadPistolBullet* bullet);
 
+    // shock wave
+    HammerWave* AcquireHammerWave();
+    void ReleaseHammerWave(HammerWave* hammerWave);
+
+    // other
+    InvisibleShockWave* AcquireInvisibleShockWave();
+    void ReleaseInvisibleShockWave(InvisibleShockWave* invisibleShockWave);
+    
 private:
     static ProjectilePool* instance;
+    // bullet
+    std::vector<Arrow*> arrows;
     std::vector<BadPistolBullet*> badPistolBullets;
+
+    // shock wave
+    std::vector<HammerWave*> hammerWaves;
+
+    // other
+    std::vector<InvisibleShockWave*> invisibleShockWaves;
 };
