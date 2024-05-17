c2d07065b8492ba100a4a6955c300905546ad0ef
CalvinWan <t110820058@ntut.org.tw>
2024-05-17 14:22:11 +0800

    [Feature Addition] Finished stage 1-4.

--- Source/Game/stage/StageFactory.cpp
+++ Source/Game/stage/StageFactory.cpp
@@ -4,6 +4,7 @@
 #include "implementation/stage_1/Stage_1_1.h"
 #include "implementation/stage_1/Stage_1_2.h"
 #include "implementation/stage_1/Stage_1_3.h"
+#include "implementation/stage_1/Stage_1_4.h"
 
 Stage* StageFactory::Create(int level, int stage) {
     if (level == 1) {
@@ -15,6 +16,9 @@ Stage* StageFactory::Create(int level, int stage) {
         }
         if (stage == 3) {
             return new Stage_1_3();
+        }
+        if (stage == 4) {
+            return new Stage_1_4();
         }
     }
     return nullptr;
---
+++ Source/Game/stage/implementation/stage_1/Stage_1_4.cpp
@@ -0,0 +1,69 @@
+﻿#include "stdafx.h"
+#include "Stage_1_4.h"
+#include "../../../manager/ObjectManager.h"
+#include "../../../manager/StageManager.h"
+#include "../../RoomSize.h"
+
+Stage_1_4::Stage_1_4() {
+    background.LoadBitmapByString({"resources/map/1-4.bmp"}, RGB(255, 255, 255));
+    centerOffset = Vec(static_cast<double>(background.GetWidth()) / 2, static_cast<double>(background.GetHeight()) / 2);
+
+    Room* room1 = new Room(Point(640, 688), centerOffset, RoomSize::MEDIUM_SIZE, 4,
+                           std::map<MonsterType, int>{{MonsterType::GOBLIN_GIANT, 1}});
+    Room* room2 = new Room(Point(1232, 720), centerOffset, RoomSize::SMALL_SIZE, 4,
+                           std::map<MonsterType, int>{{MonsterType::GOBLIN_GIANT, 1}});
+    Room* room3 = new Room(Point(0, 1328), centerOffset, RoomSize::MEDIUM_SIZE, 4,
+                           std::map<MonsterType, int>{{MonsterType::GOBLIN_GIANT, 1}});
+    Room* room4 = new Room(Point(592, 1280), centerOffset, RoomSize::LARGE_SIZE, 4,
+                           std::map<MonsterType, int>{{MonsterType::GOBLIN_GIANT, 1}});
+    Room* room5 = new Room(Point(640, 1968), centerOffset, RoomSize::MEDIUM_SIZE, 4,
+                           std::map<MonsterType, int>{{MonsterType::GOBLIN_GIANT, 1}});
+
+    rooms.push_back(room1);
+    rooms.push_back(room2);
+    rooms.push_back(room3);
+    rooms.push_back(room4);
+    rooms.push_back(room5);
+}
+
+void Stage_1_4::GenerateWall() {
+    AddWall(Point(640, 352), Point(736, 703));
+    AddWall(Point(672, 160), Point(688, 368));
+    AddWall(Point(672, 160), Point(880, 176));
+    AddWall(Point(863, 160), Point(880, 368));
+    AddWall(Point(816, 352), Point(912, 705));
+    AddWall(Point(896, 688), Point(1248, 784));
+    AddWall(Point(1231, 720), Point(1439, 736));
+    AddWall(Point(1423, 720), Point(1439, 928));
+    AddWall(Point(1231, 912), Point(1439, 928));
+    AddWall(Point(896, 864), Point(1247, 960));
+    AddWall(Point(640, 688), Point(656, 960));
+    AddWall(Point(591, 944), Point(734, 1295));
+    AddWall(Point(816, 944), Point(960, 1296));
+    AddWall(Point(944, 1280), Point(1295, 1424));
+    AddWall(Point(944, 1504), Point(1296, 1647));
+    AddWall(Point(1280, 1328), Point(1552, 1343));
+    AddWall(Point(1536, 1328), Point(1552, 1600));
+    AddWall(Point(1279, 1584), Point(1552, 1600));
+    AddWall(Point(256, 1280), Point(608, 1423));
+    AddWall(Point(255, 1505), Point(606, 1648));
+    AddWall(Point(1, 1328), Point(271, 1344));
+    AddWall(Point(1, 1328), Point(16, 1600));
+    AddWall(Point(1, 1584), Point(271, 1600));
+    AddWall(Point(592, 1632), Point(735, 1983));
+    AddWall(Point(815, 1632), Point(960, 1984));
+    AddWall(Point(640, 1969), Point(656, 2240));
+    AddWall(Point(895, 1968), Point(912, 2240));
+    AddWall(Point(641, 2224), Point(912, 2240));
+}
+
+void Stage_1_4::SetPlayerPosition() {
+    ObjectManager::Instance()->SetPlayerPosition(Point(1415, 1463) - centerOffset);
+}
+
+void Stage_1_4::SetTransferGate() {
+    StageManager::Instance()->SetTransferGatePosition(Point(775, 263) - centerOffset);
+}
+
+void Stage_1_4::GenerateObstacle() {
+}
