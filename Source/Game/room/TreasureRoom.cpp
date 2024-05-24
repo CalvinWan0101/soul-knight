c7138e59a3b0917d74331ce25c9c8a0a2d8ce632
CalvinWan <t110820058@ntut.org.tw>
2024-05-25 01:15:13 +0800

    [Feature Addition] Added TreasureRoom.

---
+++ Source/Game/room/TreasureRoom.cpp
@@ -0,0 +1,26 @@
+﻿#include "stdafx.h"
+#include "TreasureRoom.h"
+
+#include "../manager/ObjectManager.h"
+#include "../manager/StageManager.h"
+#include "../shop/Businessman.h"
+#include "../shop/TreasureChest.h"
+#include "../utils/Rand.h"
+
+TreasureRoom::TreasureRoom(Point leftTop, Vec centerOffset): Room(leftTop, RoomSize::MEDIUM_SIZE, centerOffset) {
+    switch (Rand::Instance()->Get(0, 2)) {
+    case 0:
+        treasure = new Businessman(StageManager::Instance()->GetLevelNumber(),
+                                   StageManager::Instance()->GetStageNumber());
+        break;
+    default:
+        treasure = new TreasureChest(TreasureChest::BOX_ROOM);
+        break;
+    }
+    treasure->SetPosition(topLeft + Point((this->size + 2) * 8, (this->size + 2) * 8) - centerOffset);
+    ObjectManager::Instance()->AddObject(treasure);
+}
+
+TreasureRoom::~TreasureRoom() {
+    treasure->AddTag(Tag::REMOVE_ON_NEXT_FRAME);
+}
---
+++ Source/Game/room/TreasureRoom.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "Room.h"
+#include "../utils/object/GameObject.h"
+
+class TreasureRoom : public Room {
+public:
+    TreasureRoom(Point leftTop, Vec centerOffset);
+    ~TreasureRoom();
+
+private:
+    GameObject* treasure;
+};
