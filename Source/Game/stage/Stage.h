91c2ea71fe283aced5ed0e391d579ee5548994a9
CalvinWan <t110820058@ntut.org.tw>
2024-05-24 14:16:47 +0800

    [Refactoring] Removed the unused method on Stage.

--- Source/Game/stage/Stage.h
+++ Source/Game/stage/Stage.h
@@ -20,7 +20,6 @@ public:
 protected:
     game_framework::CMovingBitmap background;
     Vec centerOffset;
-    virtual void SetMap(int level, int stage);
     virtual void GenerateWall() = 0;
     virtual void SetPlayerPosition() = 0;
     virtual void SetTransferGate() = 0;
