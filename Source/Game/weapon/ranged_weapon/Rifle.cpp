18e402ee72f68aba21905f4be923fde69e1db72b
CalvinWan <t110820058@ntut.org.tw>
2024-05-26 04:32:24 +0800

    [Bug Fixing] Fixed memory leak for Rifle.

--- Source/Game/weapon/ranged_weapon/Rifle.cpp
+++ Source/Game/weapon/ranged_weapon/Rifle.cpp
@@ -60,4 +60,5 @@ void Rifle::GenerateBullets(bool isPlayer) {
         objectManager->AddObject(bullet);
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+    delete currentRotation;
 }
