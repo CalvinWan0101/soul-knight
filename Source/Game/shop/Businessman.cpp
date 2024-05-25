0f279a84e528d3195fc1a3e0c1a3e6875236e630
ba_la_ba_la <evictor92@gmail.com>
2024-05-25 22:20:47 +0800

    [Bug Fixing] Fixed the DisplayStand exceeds the room boundaries.

--- Source/Game/shop/Businessman.cpp
+++ Source/Game/shop/Businessman.cpp
@@ -14,11 +14,11 @@ void Businessman::Start() {
     SetInteractiveText("Unmysterious businessman", InteractiveText::COMMON, -40);
     SetHitBoxByRatio(1,1);
     DisplayStand* displayStand;
-    int displayStandCount = Rand::Instance()->Get(3,5);
-    double step = 180 / static_cast<double>(displayStandCount - 1);
+    int displayStandCount = Rand::Instance()->Get(2,4);
+    double step = 140 / static_cast<double>(displayStandCount - 1);
     for (int i = 0; i < displayStandCount; i++) {
         displayStand = new DisplayStand(level, stage);
-        displayStand->SetPosition(this->position + Point(-90 + i * step, 50));
+        displayStand->SetPosition(this->position + Point(-70 + i * step, 50));
         ObjectManager::Instance()->AddObject(displayStand);
     }
 }
