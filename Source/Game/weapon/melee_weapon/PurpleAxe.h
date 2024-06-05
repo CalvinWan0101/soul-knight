a43654c9d839cda5ba7a1577a9fd7ecd363b6f7c
CalvinWan <t110820058@ntut.org.tw>
2024-06-05 20:52:43 +0800

    [Feature Addition] Added PurpleAxe.

---
+++ Source/Game/weapon/melee_weapon/PurpleAxe.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "../MeleeWeapon.h"
+
+class PurpleAxe : public MeleeWeapon {
+public:
+    PurpleAxe(double damage = 6, Point position = {0, 0});
+    Weapon* Copy() override;
+    void Start() override;
+    void Update() override;
+    void LoadResources() override;
+    void Attack() override;
+};
