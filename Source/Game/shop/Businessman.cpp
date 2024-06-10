81c067bb37aeaa5c0b9d7e552984b1c2f173793d
CalvinWan <t110820058@ntut.org.tw>
2024-06-10 16:47:14 +0800

    [Bug Fixing] Fixed the bug of DisplayStand of Businessman didn't remove after remove Businessman.

--- Source/Game/shop/Businessman.cpp
+++ Source/Game/shop/Businessman.cpp
@@ -8,26 +8,29 @@
 Businessman::Businessman(int level, int stage) : level(level), stage(stage) {
 }
 
+Businessman::~Businessman() {
+    for (DisplayStand* displayStand : displayStands) {
+        displayStand->AddTag(Tag::REMOVE_ON_NEXT_FRAME);
+    }
+}
 
 void Businessman::Start() {
     GeneralObject::Start();
     SetInteractiveText("Unmysterious businessman", InteractiveText::COMMON, -40);
-    SetHitBoxByRatio(1,1);
-    DisplayStand* displayStand;
-    int displayStandCount = Rand::Instance()->Get(2,4);
+    SetHitBoxByRatio(1, 1);
+    int displayStandCount = Rand::Instance()->Get(2, 4);
     double step = 140 / static_cast<double>(displayStandCount - 1);
     for (int i = 0; i < displayStandCount; i++) {
-        displayStand = new DisplayStand(level, stage);
+        DisplayStand* displayStand = new DisplayStand(level, stage);
         displayStand->SetPosition(this->position + Point(-70 + i * step, 50));
+        displayStands.push_back(displayStand);
         ObjectManager::Instance()->AddObject(displayStand);
     }
 }
 
 void Businessman::LoadResources() {
-    AddAnimation({"Resources/shop/businessman.bmp"}, RGB(255,255,255), 1000, false);
+    AddAnimation({"Resources/shop/businessman.bmp"}, RGB(255, 255, 255), 1000, false);
 }
 
 void Businessman::Interactive(Player* player) {
 }
-
-
--- Source/Game/shop/Businessman.h
+++ Source/Game/shop/Businessman.h
@@ -2,14 +2,19 @@
 #include "../utils/InteractableDevice.h"
 #include "../utils/object/GeneralObject.h"
 
-class Businessman : public GeneralObject, public InteractableDevice{
+class DisplayStand;
+
+class Businessman : public GeneralObject, public InteractableDevice {
 public:
     Businessman(int level, int stage);
+    ~Businessman();
     void Start() override;
     void LoadResources() override;
 
     void Interactive(Player* player) override;
+
 private:
     int level;
     int stage;
+    std::vector<DisplayStand*> displayStands;
 };
